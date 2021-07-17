c0e8428f8316f561981c6bee0aab4d6e2b670eaf
Brendan Boyle <brendanboyle87@gmail.com>
2021-07-17 08:29:49 -0400

    initial private route setup

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Login } from "./Login";
 import { Lobby } from "./Lobby";
+import { AuthProvider } from "./../contexts/Auth";
+import { PrivateRoute } from "./PrivateRoute";
 
 export default function App() {
   return (
     <div>
       <h1>Margrites</h1>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Lobby} />
-          <Route path="/login" component={Login} />
-        </Switch>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Lobby} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </AuthProvider>
       </Router>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,29 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
+import { useAuth } from "../contexts/Auth";
+import { useHistory } from "react-router-dom";
 
 export function Login() {
   const emailRef = useRef();
 
+  const { signIn } = useAuth();
+
+  const history = useHistory();
+
   async function handleSubmit(e) {
     e.preventDefault();
 
-    // @TODO: add login logic
+    // Get email and password input values
+    const email = emailRef.current.value;
+
+    // Calls `signIn` function from the context
+    const { error } = await signIn({ email });
+
+    if (error) {
+      alert("error signing in");
+    } else {
+      // Redirect user to Dashboard
+      history.push("/");
+    }
   }
 
   return (
